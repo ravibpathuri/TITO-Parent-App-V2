f3f9d60cd3aa68ca751e3dd106f870d9da5fc56d
Ravi Babu Pathuri <rpathuri@predifast.com>
2020-01-18 14:37:03 +0530

    android release changes

--- src/app/pages/transport-details/transport-details.page.ts
+++ src/app/pages/transport-details/transport-details.page.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import {NavController,LoadingController, ToastController } from '@ionic/angular';
-import { StudentSvcProvider } from '../../providers/providers';
+import { Component, OnInit } from "@angular/core";
+import {
+  NavController,
+  LoadingController,
+  ToastController
+} from "@ionic/angular";
+import { StudentSvcProvider } from "../../providers/providers";
 @Component({
-  selector: 'transport-details',
-  templateUrl: './transport-details.page.html',
-  styleUrls: ['./transport-details.page.scss'],
+  selector: "transport-details",
+  templateUrl: "./transport-details.page.html",
+  styleUrls: ["./transport-details.page.scss"]
 })
 export class TransportDetailsPage implements OnInit {
-  transportDetails= [];
+  transportDetails: any;
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     public toastCtrl: ToastController,
     public loadingController: LoadingController,
-    public vehicleSvc: StudentSvcProvider,
-    ) {
-  }
-
+    public vehicleSvc: StudentSvcProvider
+  ) {}
 
   ngOnInit() {
     let req = this.vehicleSvc.getVehicleDetails();
 
     req.subscribe((result: any) => {
-      if(result!=null)
-      {      
+      if (result != null) {
         this.transportDetails = result;
         //console.log(this.transportDetails);
       }
-    })
+    });
   }
-
 }
